bf01ccc4f98e1df55428810a11aa82333f8cc7c2
vi4me <vitalii.cherednichenko@yes.pl>
2025-10-31 16:15:58 +0100

    EK-876 Remove relation product_labels from gem

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,12 +2,6 @@
 
 module Spree
   module ProductDecorator
-    def self.prepended(base)
-      return if base.reflect_on_association(:labels)
-
-      base.has_and_belongs_to_many :labels, class_name: 'Spree::Label', join_table: 'labels_products'
-    end
-
     def first_active_label(store:)
       first_matching_label(store: store)&.name
     end
